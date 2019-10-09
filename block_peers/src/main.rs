3eb48aefde9f2102197d7caccbe56fd703d22d72
Adam Sheehan <a.t.sheehan@gmail.com>
2019-10-08 20:42:52 -0400

    Initialize window and setup game loop

--- block_peers/src/main.rs
+++ block_peers/src/main.rs
@@ -1,3 +1,64 @@
-fn main() {
-    println!("Hello, world!");
+extern crate sdl2;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use std::time::{Duration, Instant};
+
+const WINDOW_WIDTH: u32 = 800;
+const WINDOW_HEIGHT: u32 = 600;
+
+const TICKS_PER_SECOND: u64 = 60;
+const MICROSECONDS_PER_SECOND: u64 = 1_000_000;
+const MICROSECONDS_PER_TICK: u64 = MICROSECONDS_PER_SECOND / TICKS_PER_SECOND;
+
+pub fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("Block Peers", WINDOW_WIDTH, WINDOW_HEIGHT)
+        .position_centered()
+        .resizable()
+        .opengl()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    let tick_duration = Duration::from_micros(MICROSECONDS_PER_TICK);
+    let mut previous_instant = Instant::now();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Q),
+                    ..
+                } => break 'running,
+                // Handle other input here
+                _ => {}
+            }
+        }
+
+        let current_instant = Instant::now();
+        while current_instant - previous_instant >= tick_duration {
+            // Update world here
+            previous_instant += tick_duration;
+        }
+
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.clear();
+
+        // Render world here
+
+        canvas.present();
+    }
 }
