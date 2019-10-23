0240277fa1433377380bfd4191c10616095ee16e
Adam Sheehan <a.t.sheehan@gmail.com>
2019-10-23 08:10:02 -0400

    Parse host and port from args
    This adds command-line parsing to both the client and server so we can
    specify which IP and port to connect to. This will make it easier to
    test it out on an actual server without having to change the address
    in the code.
    
    To use with `cargo run` you'll need to add a `--` before passing in
    the flags.
    
    ```
    $ cargo run --bin server -- --port 4489
    $ cargo run --bin client -- --port 4489 --host 10.0.0.0
    ```

--- block_peers/src/bin/client.rs
+++ block_peers/src/bin/client.rs
@@ -1,3 +1,4 @@
+extern crate getopts;
 #[macro_use]
 extern crate log;
 extern crate rand;
@@ -5,9 +6,11 @@ extern crate sdl2;
 extern crate simplelog;
 
 // External
+use getopts::Options;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use std::net::{SocketAddr, UdpSocket};
+use std::env;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket};
 use std::time::{Duration, Instant};
 
 // Internal
@@ -22,12 +25,38 @@ const TICKS_PER_SECOND: u64 = 60;
 const MICROSECONDS_PER_SECOND: u64 = 1_000_000;
 const MICROSECONDS_PER_TICK: u64 = MICROSECONDS_PER_SECOND / TICKS_PER_SECOND;
 
+const DEFAULT_PORT: u16 = 4485;
+const DEFAULT_HOST: IpAddr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+
 pub fn main() {
     util::init_logging();
 
     // TODO: Abstract a bit/clean up
+    let args: Vec<String> = env::args().collect();
+
+    let mut opts = Options::new();
+    opts.optopt("p", "port", "connect to server on specified port (default 4485)", "PORT");
+    opts.optopt("h", "host", "connect to host at specified address (default 127.0.0.1)", "HOST");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m },
+        Err(f) => { panic!(f.to_string()) }
+    };
+
+    let port: u16 = match matches.opt_get("port") {
+        Ok(Some(port)) => { port }
+        Ok(None) => { DEFAULT_PORT }
+        Err(_) => { panic!("specified port not valid") }
+    };
+
+    let host: IpAddr = match matches.opt_get("host") {
+        Ok(Some(host)) => { host }
+        Ok(None) => { DEFAULT_HOST }
+        Err(_) => { panic!("specific host was not valid socket address") }
+    };
+
     let socket = UdpSocket::bind("0.0.0.0:0").expect("could not create a socket");
-    let server_addr: SocketAddr = "127.0.0.1:4485".parse().unwrap();
+    let server_addr = SocketAddr::new(host, port);
 
     let message = ClientMessage::Connect;
 
@@ -77,8 +106,8 @@ pub fn main() {
 
     'running: loop {
         // Check network for events
-        
-        // Input 
+
+        // Input
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. }
--- block_peers/src/bin/server.rs
+++ block_peers/src/bin/server.rs
@@ -1,15 +1,36 @@
 extern crate bincode;
+extern crate getopts;
 
 use block_peers::grid::Grid;
 use block_peers::net::{ClientMessage, ServerMessage};
 
+use getopts::Options;
+use std::env;
 use std::net::UdpSocket;
 
 const GRID_HEIGHT: u32 = 20;
 const GRID_WIDTH: u32 = 10;
 
+const DEFAULT_PORT: u16 = 4485;
+
 fn main() {
-    let socket = UdpSocket::bind("127.0.0.1:4485").expect("could not bind to port");
+    let args: Vec<String> = env::args().collect();
+
+    let mut opts = Options::new();
+    opts.optopt("p", "port", "bind to the specified port (default 4485)", "PORT");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m },
+        Err(f) => { panic!(f.to_string()) }
+    };
+
+    let port: u16 = match matches.opt_get("port") {
+        Ok(Some(port)) => { port }
+        Ok(None) => { DEFAULT_PORT }
+        Err(_) => { panic!("specified port not valid") }
+    };
+
+    let socket = UdpSocket::bind(("0.0.0.0", port)).expect("could not bind to port");
 
     let mut buffer = [0; 1000];
     let (amount, source_addr) = socket.recv_from(&mut buffer).unwrap();
