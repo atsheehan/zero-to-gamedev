092df0daa47eba1af8b21bbe8196379259d818c6
Adam Sheehan <a.t.sheehan@gmail.com>
2019-10-23 07:46:11 -0400

    Add Socket abstraction
    This wraps the UDP socket and makes it easier to serialize and
    deserialize messages without having to deal with buffers.

--- block_peers/src/bin/client.rs
+++ block_peers/src/bin/client.rs
@@ -7,11 +7,10 @@ extern crate simplelog;
 // External
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use std::net::{SocketAddr, UdpSocket};
 use std::time::{Duration, Instant};
 
 // Internal
-use block_peers::net::{ClientMessage, ServerMessage};
+use block_peers::net::{ClientMessage, ServerMessage, Socket};
 use block_peers::render::Renderer;
 use block_peers::util;
 
@@ -25,21 +24,13 @@ const MICROSECONDS_PER_TICK: u64 = MICROSECONDS_PER_SECOND / TICKS_PER_SECOND;
 pub fn main() {
     util::init_logging();
 
-    // TODO: Abstract a bit/clean up
-    let socket = UdpSocket::bind("0.0.0.0:0").expect("could not create a socket");
-    let server_addr: SocketAddr = "127.0.0.1:4485".parse().unwrap();
+    let mut socket = Socket::new().expect("could not open a new socket");
+    socket
+        .send("127.0.0.1:4485", &ClientMessage::Connect)
+        .unwrap();
 
-    let message = ClientMessage::Connect;
-
-    socket.send_to(&message.into_bytes(), &server_addr).unwrap();
-
-    let mut buffer = [0; 1000];
-    let (amount, source_addr) = socket.recv_from(&mut buffer).unwrap();
-
-    let data = &buffer[..amount];
-
-    let mut grid = match bincode::deserialize(&data) {
-        Ok(ServerMessage::Ack { grid }) => {
+    let mut grid = match socket.receive::<ServerMessage>() {
+        Ok((source_addr, ServerMessage::Ack { grid })) => {
             debug!("connected to server at {:?}", source_addr);
             grid
         }
@@ -77,8 +68,8 @@ pub fn main() {
 
     'running: loop {
         // Check network for events
-        
-        // Input 
+
+        // Input
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. }
