a99c9b47023937736fe951985899e16f7557919e
Adam Sheehan <a.t.sheehan@gmail.com>
2019-10-24 07:16:39 -0400

    Add Socket abstraction (#16)
    This wraps the UDP socket and makes it easier to serialize and
    deserialize messages without having to deal with buffers.

--- block_peers/src/bin/server.rs
+++ block_peers/src/bin/server.rs
@@ -1,28 +1,18 @@
-extern crate bincode;
-
 use block_peers::grid::Grid;
-use block_peers::net::{ClientMessage, ServerMessage};
-
-use std::net::UdpSocket;
+use block_peers::net::{ClientMessage, ServerMessage, Socket};
 
 const GRID_HEIGHT: u32 = 20;
 const GRID_WIDTH: u32 = 10;
 
 fn main() {
-    let socket = UdpSocket::bind("127.0.0.1:4485").expect("could not bind to port");
-
-    let mut buffer = [0; 1000];
-    let (amount, source_addr) = socket.recv_from(&mut buffer).unwrap();
-
-    let data = &buffer[..amount];
-
+    let mut socket = Socket::bind("0.0.0.0:4485").expect("could not create socket");
     let grid = Grid::new(GRID_HEIGHT, GRID_WIDTH);
 
-    match bincode::deserialize(&data) {
-        Ok(ClientMessage::Connect) => {
+    match socket.receive::<ClientMessage>() {
+        Ok((source_addr, ClientMessage::Connect)) => {
             println!("client at {:?} connected", source_addr);
             socket
-                .send_to(&ServerMessage::Ack { grid }.into_bytes(), &source_addr)
+                .send(source_addr, &ServerMessage::Ack { grid })
                 .unwrap();
         }
         Err(_) => {
--- block_peers/src/net.rs
+++ block_peers/src/net.rs
@@ -1,6 +1,44 @@
+use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
+use std::io::Result;
+use std::net::{SocketAddr, ToSocketAddrs, UdpSocket};
 
 use crate::grid::Grid;
+
+pub struct Socket {
+    socket: UdpSocket,
+    buffer: [u8; 1024],
+}
+
+impl Socket {
+    /// Binds a new UDP socket on any avaliable port.
+    pub fn new() -> Result<Self> {
+        Self::bind("0.0.0.0:0")
+    }
+
+    /// Binds a new UDP socket to the specific socket address.
+    pub fn bind<A: ToSocketAddrs>(addr: A) -> Result<Self> {
+        let buffer = [0; 1024];
+        UdpSocket::bind(addr).map(|socket| Socket { socket, buffer })
+    }
+
+    pub fn receive<D: DeserializeOwned>(&mut self) -> Result<(SocketAddr, D)> {
+        let (bytes_received, source_addr) = match self.socket.recv_from(&mut self.buffer) {
+            Ok(result) => result,
+            Err(e) => return Err(e),
+        };
+
+        let data = &self.buffer[..bytes_received];
+        Ok((source_addr, bincode::deserialize(&data).unwrap()))
+    }
+
+    pub fn send<A: ToSocketAddrs, S: Serialize>(&mut self, addr: A, message: S) -> Result<()> {
+        let serialized_message = bincode::serialize(&message).unwrap();
+        self.socket.send_to(&serialized_message, addr)?;
+
+        Ok(())
+    }
+}
 
 #[derive(Copy, Clone, Serialize, Deserialize, Debug)]
 pub enum ClientMessage {
