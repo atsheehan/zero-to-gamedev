ddf6e6e010603576f1f34789cb7f49f31715e239
Stefan VanBuren <vanburenstefan@gmail.com>
2019-11-12 13:28:17 -0500

    Rework render signature

--- block_peers/src/grid.rs
+++ block_peers/src/grid.rs
@@ -124,11 +124,11 @@ impl Grid {
             match self.cells[idx] {
                 Brick::Occupied(brick_type) => {
                     let image = Image::from_brick_type(brick_type);
-                    renderer.render_image(image, cell.into(), Opacity::Opaque);
+                    renderer.render_image(image, cell, Opacity::Opaque);
                 }
                 Brick::Breaking(frame) => {
                     let image = Image::from_brick_type(BrickType::Smoke(frame));
-                    renderer.render_image(image, cell.into(), Opacity::Opaque);
+                    renderer.render_image(image, cell, Opacity::Opaque);
                 }
                 _ => {}
             }
@@ -258,7 +258,7 @@ impl Grid {
 
     fn render_piece(&self, renderer: &mut Renderer, piece: &Piece, opacity: Opacity) {
         for cell in piece.global_iter() {
-            renderer.render_image(piece.image(), cell.into(), opacity);
+            renderer.render_image(piece.image(), cell, opacity);
         }
     }
 
